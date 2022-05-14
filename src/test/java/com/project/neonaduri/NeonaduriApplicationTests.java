3f2fef00eaae87cac24075bfabe34bbcc64ffb2b
SeungHo Baek <seungho1216@naver.com>
2022-05-14 19:42:39 +0900

    new commit

---
+++ src/test/java/com/project/neonaduri/NeonaduriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.neonaduri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeonaduriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
