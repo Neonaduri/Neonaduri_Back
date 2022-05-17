64f002c7c5dbe854d1454042369f08b15206403d
Baek Seungho <seungho1216@naver.com>
2022-05-18 02:01:31 +0900

    yml 암호화

--- src/main/java/com/project/neonaduri/NeonaduriApplication.java
+++ src/main/java/com/project/neonaduri/NeonaduriApplication.java
@@ -1,19 +1,13 @@
 package com.project.neonaduri;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class NeonaduriApplication {
 
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.properties,"
-            + "classpath:aws.yml";
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(NeonaduriApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
+        SpringApplication.run(NeonaduriApplication.class, args);
     }
 
 }
--- src/main/java/com/project/neonaduri/config/AmazonS3Config.java
+++ src/main/java/com/project/neonaduri/config/AmazonS3Config.java
@@ -1,4 +1,4 @@
-package com.project.neonaduri.common.image.config;
+package com.project.neonaduri.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
@@ -22,6 +22,7 @@ public class AmazonS3Config {
 
     @Bean
     public AmazonS3Client amazonS3Client() {
+
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(region)
