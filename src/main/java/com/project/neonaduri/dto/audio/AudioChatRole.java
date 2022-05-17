7e3b6a1ffa8ea1d70e71746361e94b77941a7c9c
Baek Seungho <seungho1216@naver.com>
2022-05-17 22:13:12 +0900

    재시작

--- src/main/java/com/project/neonaduri/dto/audio/AudioChatRole.java
+++
@@ -1,6 +0,0 @@
-package com.project.neonaduri.dto.audio;
-
-
-public enum AudioChatRole {
-    MODERATOR, PUBLISHER, SUBSCRIBER
-}
--- src/main/java/com/project/neonaduri/repository/DaysRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.neonaduri.repository;
-
-import com.project.neonaduri.model.Days;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DaysRepository extends JpaRepository<Days, Long> {
-
-
-}
