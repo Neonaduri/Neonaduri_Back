3432f8f7502ef2d20a1aec9871aa9dd8faa70058
Baek Seungho <seungho1216@naver.com>
2022-05-18 02:01:51 +0900

    yml 암호화

---
+++ src/main/java/com/project/neonaduri/config/JasyptConfig.java
@@ -0,0 +1,32 @@
+package com.project.neonaduri.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String encryptKey;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor(){
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(encryptKey);
+        config.setPoolSize("1");
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setStringOutputType("base64");
+        config.setKeyObtentionIterations("1000");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        //config를 바탕으로 암호화 진행
+        encryptor.setConfig(config);
+
+        //암호화 결과물 return
+        return encryptor;
+    }
+}
