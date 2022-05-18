e843977de6404eb449d7d4b3b85812b96ca08d35
Baek Seungho <seungho1216@naver.com>
2022-05-18 15:53:23 +0900

    cicd7

--- src/test/java/com/project/neonaduri/JasyptTest.java
+++ src/test/java/com/project/neonaduri/JasyptTest.java
@@ -1,6 +1,5 @@
 package com.project.neonaduri;
 
-import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.junit.jupiter.api.Test;
@@ -9,30 +8,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class JasyptTest {
 
-    @Test
-    public void stringEncryptor(){
-        String text="평문비밀번호";
-
-        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        System.out.println(System.getenv("JASYPT_PASSWORD"));
-        config.setPassword(System.getenv("JASYPT_PASSWORD"));
-//        config.setPassword("sjskemfdl");
-        config.setPoolSize("1");
-        config.setAlgorithm("PBEWithMD5AndDES");
-        config.setStringOutputType("base64");
-        config.setKeyObtentionIterations("1000");
-        config.setProviderName("SunJCE");
-        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
-        //config를 바탕으로 암호화 진행
-        encryptor.setConfig(config);
-
-        String encryptText=encryptor.encrypt(text);
-        System.out.println(encryptText);
-        String decryptText= encryptor.decrypt(encryptText);
-        System.out.println(decryptText);
-        assertThat(text).isEqualTo(decryptText);
-    }
+//    @Test
+//    public void stringEncryptor(){
+//        String text="평문비밀번호";
+//
+//        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+//        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+//        System.out.println(System.getenv("JASYPT_PASSWORD"));
+//        config.setPassword(System.getenv("JASYPT_PASSWORD"));
+////        config.setPassword("sjskemfdl");
+//        config.setPoolSize("1");
+//        config.setAlgorithm("PBEWithMD5AndDES");
+//        config.setStringOutputType("base64");
+//        config.setKeyObtentionIterations("1000");
+//        config.setProviderName("SunJCE");
+//        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+//        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+//        //config를 바탕으로 암호화 진행
+//        encryptor.setConfig(config);
+//
+//        String encryptText=encryptor.encrypt(text);
+//        System.out.println(encryptText);
+//        String decryptText= encryptor.decrypt(encryptText);
+//        System.out.println(decryptText);
+//        assertThat(text).isEqualTo(decryptText);
+//    }
 
 }
