c06c3b01e6617cbe9a30e77e10be59b225d4caac
Baek Seungho <seungho1216@naver.com>
2022-05-18 20:00:12 +0900

    cicd14

--- src/main/java/com/project/neonaduri/config/AmazonS3Config.java
+++ src/main/java/com/project/neonaduri/config/AmazonS3Config.java
@@ -22,7 +22,7 @@ public class AmazonS3Config {
 
     @Bean
     public AmazonS3Client amazonS3Client() {
-        System.out.println(accessKey);
+
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(region)
--- src/main/java/com/project/neonaduri/config/JasyptConfig.java
+++ src/main/java/com/project/neonaduri/config/JasyptConfig.java
@@ -6,6 +6,7 @@ import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class JasyptConfig {
 
